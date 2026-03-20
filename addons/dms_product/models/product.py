46e446703b45ede7af1ca26431305d5900ef81f9
AudiChang <AudiChang-hub@users.noreply.github.com>
2026-03-20 14:00:07 +0800

    feat(dms_product): kanban 設定加入全部欄位 + 儲存按鈕
    - kanban_config.py：新增油車、電車、車身所有規格欄位共 28 個可設定項
    - product.py：對應新增 kanban_cfg_* computed boolean 旗標
    - kanban_config_views.xml：加入「儲存設定」按鈕 + 欄位分群顯示（基本資料/油車/電車/車身）
    - product_views.xml：kanban 模板加入所有新欄位條件渲染

--- addons/dms_product/models/product.py
+++ addons/dms_product/models/product.py
@@ -28,6 +28,31 @@ class DmsProduct(models.Model):
     kanban_cfg_energy_type = fields.Boolean(compute='_compute_kanban_cfg', store=False)
     kanban_cfg_color = fields.Boolean(compute='_compute_kanban_cfg', store=False)
     kanban_cfg_color_code = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    # 動力規格（油車）
+    kanban_cfg_engine_displacement = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    kanban_cfg_fuel_tank = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    kanban_cfg_engine_type = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    kanban_cfg_consumption_grade = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    kanban_cfg_efficiency = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    kanban_cfg_max_hp = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    kanban_cfg_max_torque = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    # 動力規格（電車）
+    kanban_cfg_power_system = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    kanban_cfg_max_output = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    kanban_cfg_ev_max_hp = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    kanban_cfg_ev_max_torque = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    kanban_cfg_ev_efficiency = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    kanban_cfg_transmission = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    kanban_cfg_battery_capacity = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    kanban_cfg_battery_type = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    kanban_cfg_charge_time = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    # 車身規格
+    kanban_cfg_dimensions = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    kanban_cfg_seat_height = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    kanban_cfg_wheel_base = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    kanban_cfg_vehicle_weight = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    kanban_cfg_tire_front = fields.Boolean(compute='_compute_kanban_cfg', store=False)
+    kanban_cfg_tire_rear = fields.Boolean(compute='_compute_kanban_cfg', store=False)
 
     def _compute_kanban_cfg(self):
         config = self.env['dms.kanban.product.config'].sudo().search(
@@ -41,6 +66,28 @@ class DmsProduct(models.Model):
                 kanban_cfg_energy_type=config.show_energy_type,
                 kanban_cfg_color=config.show_color,
                 kanban_cfg_color_code=config.show_color_code,
+                kanban_cfg_engine_displacement=config.show_engine_displacement,
+                kanban_cfg_fuel_tank=config.show_fuel_tank,
+                kanban_cfg_engine_type=config.show_engine_type,
+                kanban_cfg_consumption_grade=config.show_consumption_grade,
+                kanban_cfg_efficiency=config.show_efficiency,
+                kanban_cfg_max_hp=config.show_max_hp,
+                kanban_cfg_max_torque=config.show_max_torque,
+                kanban_cfg_power_system=config.show_power_system,
+                kanban_cfg_max_output=config.show_max_output,
+                kanban_cfg_ev_max_hp=config.show_ev_max_hp,
+                kanban_cfg_ev_max_torque=config.show_ev_max_torque,
+                kanban_cfg_ev_efficiency=config.show_ev_efficiency,
+                kanban_cfg_transmission=config.show_transmission,
+                kanban_cfg_battery_capacity=config.show_battery_capacity,
+                kanban_cfg_battery_type=config.show_battery_type,
+                kanban_cfg_charge_time=config.show_charge_time,
+                kanban_cfg_dimensions=config.show_dimensions,
+                kanban_cfg_seat_height=config.show_seat_height,
+                kanban_cfg_wheel_base=config.show_wheel_base,
+                kanban_cfg_vehicle_weight=config.show_vehicle_weight,
+                kanban_cfg_tire_front=config.show_tire_front,
+                kanban_cfg_tire_rear=config.show_tire_rear,
             )
         else:
             cfg = dict(
@@ -50,6 +97,28 @@ class DmsProduct(models.Model):
                 kanban_cfg_energy_type=True,
                 kanban_cfg_color=True,
                 kanban_cfg_color_code=False,
+                kanban_cfg_engine_displacement=False,
+                kanban_cfg_fuel_tank=False,
+                kanban_cfg_engine_type=False,
+                kanban_cfg_consumption_grade=False,
+                kanban_cfg_efficiency=False,
+                kanban_cfg_max_hp=False,
+                kanban_cfg_max_torque=False,
+                kanban_cfg_power_system=False,
+                kanban_cfg_max_output=False,
+                kanban_cfg_ev_max_hp=False,
+                kanban_cfg_ev_max_torque=False,
+                kanban_cfg_ev_efficiency=False,
+                kanban_cfg_transmission=False,
+                kanban_cfg_battery_capacity=False,
+                kanban_cfg_battery_type=False,
+                kanban_cfg_charge_time=False,
+                kanban_cfg_dimensions=False,
+                kanban_cfg_seat_height=False,
+                kanban_cfg_wheel_base=False,
+                kanban_cfg_vehicle_weight=False,
+                kanban_cfg_tire_front=False,
+                kanban_cfg_tire_rear=False,
             )
         for rec in self:
             for key, val in cfg.items():

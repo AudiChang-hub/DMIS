11119d4d5f9a5a7f7c503de3c6640f07f25e1ce9
AudiChang-hub <hongsian.c@gmail.com>
2026-07-28 18:52:16 +0800

    改為手機配件單欄排列

--- sales/tests/test_order_flow.py
+++ sales/tests/test_order_flow.py
@@ -134,6 +134,7 @@ class OrderFlowTests(TestCase):
             content.index('id="add-accessory"'),
             content.index('id="accessory-forms"'),
         )
+        self.assertContains(order_response, 'class="accessory-row"')
         self.assertContains(order_response, "或從手機相簿選擇圖片")
         self.assertContains(order_response, "＋ 新增費用")
         self.assertContains(order_response, "現金")
